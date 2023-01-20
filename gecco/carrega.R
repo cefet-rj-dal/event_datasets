fb8379e62fc4f75fc5d9f8e17b4a6e61f6f2ef9b
Janio de Souza Lima <56620033+janiosl@users.noreply.github.com>
2023-01-20 17:25:54 -0300

    Update carrega.R

--- gecco/carrega.R
+++ gecco/carrega.R
@@ -2,19 +2,24 @@ library(EventDetectR)
 library(dplyr)
 
 carrega <- function() {
+  #Dataset origin
   gecco <- geccoIC2018Train
   row.names(gecco) <- gecco$Time
+  #Event labels
   reference <- subset(gecco, select=c(EVENT))
+  #Series without labels or time column
   data <- subset(gecco, select = -c(EVENT, Time))
 
+  #Dataset organized with series, divided into a list
   dataset <- list()
   i = 1
   
   for (i in 1:length(data)) {
-    serie <- data %>% select(i)
+    serie <- data %>% select(all_of(i))
     dataset <- c(dataset, serie)
   }
   
+  #Adding the labels to the end of the dataset
   dataset <- c(dataset, reference)
   
   return(dataset)
