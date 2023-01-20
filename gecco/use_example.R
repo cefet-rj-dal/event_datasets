3142468bb063189da17445435abbfb709162c99a
Janio de Souza Lima <56620033+janiosl@users.noreply.github.com>
2023-01-20 17:26:05 -0300

    Create use_example.R

---
+++ gecco/use_example.R
@@ -0,0 +1,41 @@
+#install.packages("strucchange")
+library(strucchange)
+#install.packages("TSA")
+library(TSA)
+#install.packages("urca")
+library(urca)
+#install.packages("mFilter")
+library(mFilter)
+
+
+source("carrega.R")
+data <- carrega()
+#Select subset one day with syntethic events
+reference <- data$EVENT[16500:18000]
+test <- data$pH[16500:18000]
+
+
+#Plotting original serie
+plot(ts(test),type="l",
+     ylab="value",
+     xlab="time")
+
+#Autocorrelation function
+plot(TSA::acf(ts(test), plot=FALSE, na.action = na.pass), main="")
+
+
+
+#Stationarity analysis
+#Unit root identification in the series
+#ADF test
+adf_res <- ur.df(ts(test),
+                type = "trend",
+                lags = 30,
+                selectlags = "AIC"
+                )
+
+summary(adf_res)
+#Compare test statistic values with critical values
+#If value teststat > cval => Reject the null hypothesis ==> That is, the series is non-stationary
+summary(adf_res)@teststat
+summary(adf_res)@cval
