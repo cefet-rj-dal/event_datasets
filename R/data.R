51f3c129153061707b5f6231f340006a94a684b6
Janio de Souza Lima <56620033+janiosl@users.noreply.github.com>
2023-05-03 22:24:03 -0300

    Create data.R

---
+++ R/data.R
@@ -0,0 +1,344 @@
+#' @name yahoo_a1
+#' @title A1 Yahoo Benchmark Dataset for Time Series Anomaly Detection
+#' @description A1 Group --> Real data from Yahoo services internet traffic.
+#'     Data collected from user activity and server logs from Yahoo services.
+#'     Internet traffic and synthetic data. Creation Date	2015.
+#'     See \href{https://github.com/cefet-rj-dal/event_datasets}{cefet-rj-dal/event_datasets}
+#'     for detailed guidance on using this package and the other datasets available in it.
+#'     Labels available? Yes
+#'
+#' @docType data
+#' @usage data(yahoo_a1)
+#' @format A list of time series.
+#' @keywords datasets
+#'
+#' @references Yahoo Webscope data-sharing program
+#' @source \href{https://yahooresearch.tumblr.com/post/114590420346/a-benchmark-dataset-for-time-series-anomaly}{Yahoo Labs}
+#'
+#' @examples
+#' data(yahoo_a1)
+#' serie <- yahoo_a1[[1]]
+"yahoo_a1"
+
+#' @name yahoo_a2
+#' @title A2 Yahoo Benchmark Dataset for Time Series Anomaly Detection
+#' @description A2 Group --> Synthetic data to simulate difficulties on detecting outliers and change points.
+#'     Data collected from user activity and server logs from Yahoo services.
+#'     Internet traffic and synthetic data. Creation Date	2015.
+#'     See \href{https://github.com/cefet-rj-dal/event_datasets}{cefet-rj-dal/event_datasets}
+#'     for detailed guidance on using this package and the other datasets available in it.
+#'     Labels available? Yes
+#'
+#' @docType data
+#' @usage data(yahoo_a2)
+#' @format A list of time series.
+#' @keywords datasets
+#'
+#' @references Yahoo Webscope data-sharing program
+#' @source \href{https://yahooresearch.tumblr.com/post/114590420346/a-benchmark-dataset-for-time-series-anomaly}{Yahoo Labs}
+#'
+#' @examples
+#' data(yahoo_a2)
+#' serie <- yahoo_a2[[1]]
+"yahoo_a2"
+
+
+#' @name yahoo_a3
+#' @title A3 Yahoo Benchmark Dataset for Time Series Anomaly Detection
+#' @description A3 Group --> Real data from Yahoo services internet traffic.
+#'     Data collected from user activity and server logs from Yahoo services.
+#'     Internet traffic and synthetic data. Creation Date	2015.
+#'     See \href{https://github.com/cefet-rj-dal/event_datasets}{cefet-rj-dal/event_datasets}
+#'     for detailed guidance on using this package and the other datasets available in it.
+#'     Labels available? Yes
+#'
+#' @docType data
+#' @usage data(yahoo_a3)
+#' @format A list of time series.
+#' @keywords datasets
+#'
+#' @references Yahoo Webscope data-sharing program
+#' @source \href{https://yahooresearch.tumblr.com/post/114590420346/a-benchmark-dataset-for-time-series-anomaly}{Yahoo Labs}
+#'
+#' @examples
+#' data(yahoo_a3)
+#' serie <- yahoo_a3[[1]]
+"yahoo_a3"
+
+
+#' @name yahoo_a4
+#' @title A4 Yahoo Benchmark Dataset for Time Series Anomaly Detection
+#' @description A4 Group --> Real data from Yahoo services internet traffic.
+#'     Data collected from user activity and server logs from Yahoo services.
+#'     Internet traffic and synthetic data. Creation Date	2015.
+#'     See \href{https://github.com/cefet-rj-dal/event_datasets}{cefet-rj-dal/event_datasets}
+#'     for detailed guidance on using this package and the other datasets available in it.
+#'     Labels available? Yes
+#'
+#' @docType data
+#' @usage data(yahoo_a4)
+#' @format A list of time series.
+#' @keywords datasets
+#'
+#' @references Yahoo Webscope data-sharing program
+#' @source \href{https://yahooresearch.tumblr.com/post/114590420346/a-benchmark-dataset-for-time-series-anomaly}{Yahoo Labs}
+#'
+#' @examples
+#' data(yahoo_a4)
+#' serie <- yahoo_a4[[1]]
+"yahoo_a4"
+
+
+#' @name oil_3w_Type_1
+#' @title Oil wells dataset - Type 1
+#' @description
+#' Firts realistic dataset with real events in oil well drilling. The data available
+#'     in this package consist of time series already analyzed and applied in
+#'     research experiments by the DAL group (Data Analytics Lab). The series are
+#'     divided into 7 groups (Type_0, Type_1, Type_2, Type_5, Type_6, Type_7 and Type_8).
+#'     Creation date: 2019.
+#'     See \href{https://github.com/cefet-rj-dal/event_datasets}{cefet-rj-dal/event_datasets}
+#'     for detailed guidance on using this package and the other datasets available in it.
+#'     Labels available? Yes
+#'
+#'
+#' @docType data
+#' @usage data(oil_3w_Type_1)
+#' @format A list of time series.
+#' @keywords datasets
+#'
+#'#' @references 3W dataset Data Set
+#' @source \href{https://archive.ics.uci.edu/ml/datasets/3W+dataset}{UCI Machine Learning Repository}
+#'
+#' @examples
+#' data(oil_3w_Type_1)
+#' serie <- oil_3w_Type_1[[1]]
+"oil_3w_Type_1"
+
+
+#' @name oil_3w_Type_2
+#' @title Oil wells dataset - Type 2
+#' @description
+#' Firts realistic dataset with real events in oil well drilling. The data available
+#'     in this package consist of time series already analyzed and applied in
+#'     research experiments by the DAL group (Data Analytics Lab). The series are
+#'     divided into 7 groups (Type_0, Type_1, Type_2, Type_5, Type_6, Type_7 and Type_8).
+#'     Creation date: 2019.
+#'     See \href{https://github.com/cefet-rj-dal/event_datasets}{cefet-rj-dal/event_datasets}
+#'     for detailed guidance on using this package and the other datasets available in it.
+#'     Labels available? Yes
+#'
+#'
+#' @docType data
+#' @usage data(oil_3w_Type_2)
+#' @format A list of time series.
+#' @keywords datasets
+#'
+#'#' @references 3W dataset Data Set
+#' @source \href{https://archive.ics.uci.edu/ml/datasets/3W+dataset}{UCI Machine Learning Repository}
+#'
+#' @examples
+#' data(oil_3w_Type_2)
+#' serie <- oil_3w_Type_2[[1]]
+"oil_3w_Type_2"
+
+
+#' @name oil_3w_Type_5
+#' @title Oil wells dataset - Type 5
+#' @description
+#' Firts realistic dataset with real events in oil well drilling. The data available
+#'     in this package consist of time series already analyzed and applied in
+#'     research experiments by the DAL group (Data Analytics Lab). The series are
+#'     divided into 7 groups (Type_0, Type_1, Type_2, Type_5, Type_6, Type_7 and Type_8).
+#'     Creation date: 2019.
+#'     See \href{https://github.com/cefet-rj-dal/event_datasets}{cefet-rj-dal/event_datasets}
+#'     for detailed guidance on using this package and the other datasets available in it.
+#'     Labels available? Yes
+#'
+#'
+#' @docType data
+#' @usage data(oil_3w_Type_5)
+#' @format A list of time series.
+#' @keywords datasets
+#'
+#'#' @references 3W dataset Data Set
+#' @source \href{https://archive.ics.uci.edu/ml/datasets/3W+dataset}{UCI Machine Learning Repository}
+#'
+#' @examples
+#' data(oil_3w_Type_5)
+#' serie <- oil_3w_Type_5[[1]]
+"oil_3w_Type_5"
+
+
+#' @name oil_3w_Type_6
+#' @title Oil wells dataset - Type 6
+#' @description
+#' Firts realistic dataset with real events in oil well drilling. The data available
+#'     in this package consist of time series already analyzed and applied in
+#'     research experiments by the DAL group (Data Analytics Lab). The series are
+#'     divided into 7 groups (Type_0, Type_1, Type_2, Type_5, Type_6, Type_7 and Type_8).
+#'     Creation date: 2019.
+#'     See \href{https://github.com/cefet-rj-dal/event_datasets}{cefet-rj-dal/event_datasets}
+#'     for detailed guidance on using this package and the other datasets available in it.
+#'     Labels available? Yes
+#'
+#'
+#' @docType data
+#' @usage data(oil_3w_Type_6)
+#' @format A list of time series.
+#' @keywords datasets
+#'
+#'#' @references 3W dataset Data Set
+#' @source \href{https://archive.ics.uci.edu/ml/datasets/3W+dataset}{UCI Machine Learning Repository}
+#'
+#' @examples
+#' data(oil_3w_Type_6)
+#' serie <- oil_3w_Type_6[[1]]
+"oil_3w_Type_6"
+
+
+#' @name oil_3w_Type_7
+#' @title Oil wells dataset - Type 7
+#' @description
+#' Firts realistic dataset with real events in oil well drilling. The data available
+#'     in this package consist of time series already analyzed and applied in
+#'     research experiments by the DAL group (Data Analytics Lab). The series are
+#'     divided into 7 groups (Type_0, Type_1, Type_2, Type_5, Type_6, Type_7 and Type_8).
+#'     Creation date: 2019.
+#'     See \href{https://github.com/cefet-rj-dal/event_datasets}{cefet-rj-dal/event_datasets}
+#'     for detailed guidance on using this package and the other datasets available in it.
+#'     Labels available? Yes
+#'
+#'
+#' @docType data
+#' @usage data(oil_3w_Type_7)
+#' @format A list of time series.
+#' @keywords datasets
+#'
+#'#' @references 3W dataset Data Set
+#' @source \href{https://archive.ics.uci.edu/ml/datasets/3W+dataset}{UCI Machine Learning Repository}
+#'
+#' @examples
+#' data(oil_3w_Type_7)
+#' serie <- oil_3w_Type_7[[1]]
+"oil_3w_Type_7"
+
+
+#' @name oil_3w_Type_8
+#' @title Oil wells dataset - Type 8
+#' @description
+#' Firts realistic dataset with real events in oil well drilling. The data available
+#'     in this package consist of time series already analyzed and applied in
+#'     research experiments by the DAL group (Data Analytics Lab). The series are
+#'     divided into 7 groups (Type_0, Type_1, Type_2, Type_5, Type_6, Type_7 and Type_8).
+#'     Creation date: 2019.
+#'     See \href{https://github.com/cefet-rj-dal/event_datasets}{cefet-rj-dal/event_datasets}
+#'     for detailed guidance on using this package and the other datasets available in it.
+#'     Labels available? Yes
+#'
+#'
+#' @docType data
+#' @usage data(oil_3w_Type_8)
+#' @format A list of time series.
+#' @keywords datasets
+#'
+#'#' @references 3W dataset Data Set
+#' @source \href{https://archive.ics.uci.edu/ml/datasets/3W+dataset}{UCI Machine Learning Repository}
+#'
+#' @examples
+#' data(oil_3w_Type_8)
+#' serie <- oil_3w_Type_8[[1]]
+"oil_3w_Type_8"
+
+
+#' @name gecco
+#' @title Gecco Challenge 2018
+#' @description Data collection for water quality monitoring.
+#'     Data Type:	Water quality. Category:	Environment. Creation Date	2018.
+#'     See \href{https://github.com/cefet-rj-dal/event_datasets}{cefet-rj-dal/event_datasets}
+#'     for detailed guidance on using this package and the other datasets available in it.
+#'     Labels available? Yes
+#'
+#' @docType data
+#' @usage data(gecco)
+#' @format A list of time series.
+#' @keywords datasets
+#'
+#'#' @references Genetic and Evolutionary Computation Conference (Gecco), Association for Computing Machinery (ACM)
+#'
+#' @source \href{https://www.spotseven.de/gecco/gecco-challenge}{Gecco Challenge 2018}
+#'
+#' @examples
+#' data(gecco)
+#' serie <- gecco[[1]]
+"gecco"
+
+
+#' @name mortality_cnes
+#' @title Child mortality dataset
+#' @description Child mortality dataset. Data for assessing infant mortality,
+#'     an important health indicator.
+#'     See \href{https://github.com/cefet-rj-dal/event_datasets}{cefet-rj-dal/event_datasets}
+#'     for detailed guidance on using this package and the other datasets available in it.
+#'     Labels available? Yes
+#'
+#' @docType data
+#' @usage data(mortality_cnes)
+#' @format A list of time series.
+#' @keywords datasets
+#'
+#'
+#' @examples
+#' data(mortality_cnes)
+#' serie <- mortality_cnes[[1]]
+"mortality_cnes"
+
+
+
+#' @name eia_oil_prices
+#' @title EIA Oil Spot Prices
+#' @description
+#' Spot prices of oil and other liquids such as gasoline, heating oil, diesel, jet fuel and propane.
+#'     Crude oil in Dollars per barrel, other products in Dollars per gallon. Prices are calculated
+#'     by EIA from daily data by taking an unweighted average of the daily closing spot prices
+#'     for a given product over the specified time period.
+#'     See \href{https://github.com/cefet-rj-dal/event_datasets}{cefet-rj-dal/event_datasets}
+#'     for detailed guidance on using this package and the other datasets available in it.
+#'     Labels available? No
+#'
+#'
+#' @docType data
+#' @usage data(eia_oil_prices)
+#' @format A list of time series.
+#' @keywords datasets
+#'
+#'#' @references EIA Energy Information Administration
+#' @source \href{https://www.eia.gov/dnav/pet/pet_pri_spt_s1_d.htm}{EIA Energy Information Administration}
+#'
+#' @examples
+#' data(eia_oil_prices)
+#' serie <- eia_oil_prices[[1]]
+"eia_oil_prices"
+
+
+#' @name fi_br
+#' @title Finance Indexes
+#' @description
+#' Finance: Securities Price Indexes (Brazil).
+#'     See \href{https://github.com/cefet-rj-dal/event_datasets}{cefet-rj-dal/event_datasets}
+#'     for detailed guidance on using this package and the other datasets available in it.
+#'     Labels available? Yes
+#'
+#'
+#' @docType data
+#' @usage data(fi_br)
+#' @format A list of time series.
+#' @keywords datasets
+#'
+#'#'#' @references Various public sources (b3, Br Investing, Central Bank of Brazil)
+#' @source \href{https://github.com/cefet-rj-dal/event_datasets/raw/main/original_datasets/fi_br/data/glossary.xlsx}{Glossary with complete references}
+#'
+#' @examples
+#' data(fi_br)
+#' serie <- fi_br[[1]]
+"fi_br"
