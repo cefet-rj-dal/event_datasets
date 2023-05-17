7ba1d129f4d54b98ead27297bb5a306e9f9bc9e7
Janio de Souza Lima <56620033+janiosl@users.noreply.github.com>
2023-05-17 09:04:19 -0300

    Update build_package.R

--- develop/build_package.R
+++ develop/build_package.R
@@ -17,28 +17,3 @@ if (FALSE) {
   load_all()
 }
 
-
-
-# Usage examples ----------------------------------------------------------
-?dalevents
-
-data(yahoo_a1)
-?yahoo_a1
-
-data(oil_3w_Type_1)
-?oil_3w_Type_1
-
-data(gecco)
-?gecco
-
-data(mortality_cnes)
-?mortality_cnes
-
-data(eia_oil_prices)
-?eia_oil_prices
-
-data(fi_br)
-?fi_br
-
-data(numenta_grp_all)
-?numenta_grp_all
